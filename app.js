f4bab4be241edede82cb4ee65e92398ca828e4fe
jamisultani <sultanijami03@gmail.com>
2024-11-12 19:29:18 +0500

    now ready to commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// alert("Hello World");
+
+// var a = 4;
+// var b = 1;
+// var c = a+b;
+// alert(c)
+
+var a = 4;
+var b = 1;
+var c = a+b;
+console.log(c);
+
+// var x = "muhammad";
+// var y = "jami";
+// var z = x+y;
+// alert(z);
+
+// var f = 30;
+// var g = 2;
+// var h = f*g;
+// alert(h)
+
+// var a = 3;
+// var b = 5;
+// var c = 7;
+// var d = (b - a) * c;
+// alert(d);
+
+// var a = 5;
+// var b = a++;
+// alert(b);
+
+
+var x = 10;
+var y = x++ + ++x - x + x++ - ++x +x;
+alert(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
